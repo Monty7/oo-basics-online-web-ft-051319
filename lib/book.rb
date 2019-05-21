8fda3223f3e699703707c93d0aaa7aa0eda18cfe
M Banks <montensia1@outlook.com>
2019-05-21 15:34:21 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,30 +1,14 @@
 class Book
-  attr_accessor :title
-  
-  def author=(author)
-    @author = author
+  def initialize(title)
+    @title = title
   end
+  
+  def title
+    @title
+  end
+  
+  attr_accessor :author :page_count :genre
 
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @page_count = num
-  end
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
-  end
-  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
